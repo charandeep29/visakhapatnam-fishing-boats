ecfde708999b5a80949af6fdd83a567013d7909c
charandeep29 <cdooda2@gitam.in>
2026-06-09 09:44:21 +0530

    Add files via upload

--- firebase.js
+++ firebase.js
@@ -7,14 +7,14 @@ import {
 } from "https://www.gstatic.com/firebasejs/12.4.0/firebase-firestore.js";
 
 const firebaseConfig = {
-  apiKey: "YOUR_API_KEY",
+  apiKey: "AIzaSyAXEedkrLZbE3o5ktkICnU1ixyUqgGLWZM",
   authDomain: "vizag-fisherman-tracker.firebaseapp.com",
   projectId: "vizag-fisherman-tracker",
   storageBucket: "vizag-fisherman-tracker.firebasestorage.app",
   messagingSenderId: "39728715949",
-  appId: "1:39728715949:web:594959d9b33998e8a134e4"
+  appId: "1:39728715949:web:594959d9b33998e8a134e4",
+  measurementId: "G-HE52Y2LQ7Y"
 };
-
 const app = initializeApp(firebaseConfig);
 const db = getFirestore(app);
 
